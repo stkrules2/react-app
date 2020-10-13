4793f182d7f2bacba6094a525ffeb5047986a42d
stkrules2 <sulmanazhar10@gmail.com>
2020-10-13 05:47:03 +0500

    Updates

---
+++ precache-manifest.69de10bf1e6eb00215de4227bc54a0ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244ff65d111a89c644e5f8b4e3733149",
+    "url": "/react-first-app/index.html"
+  },
+  {
+    "revision": "504b40a87e4a6dea3d3f",
+    "url": "/react-first-app/static/css/main.09e87567.chunk.css"
+  },
+  {
+    "revision": "bc4f0aec8753b31eee9d",
+    "url": "/react-first-app/static/js/2.e601ea17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-first-app/static/js/2.e601ea17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504b40a87e4a6dea3d3f",
+    "url": "/react-first-app/static/js/main.abf4a5fc.chunk.js"
+  },
+  {
+    "revision": "53968d5ea448d99793d4",
+    "url": "/react-first-app/static/js/runtime-main.d6d20f4c.js"
+  }
+]);
